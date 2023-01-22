3626bb14b5157817a04a2dcb452e533110589a4f
harry <harrisongitau3@gmail.com>
2023-01-22 18:13:51 +0300

    Style: Add moment JS for time formatting

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from config import Config
 
 
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
 
 
 from app import routes, models, errors
