562ad710b174c719b02085f09ceb91ee1470099c
harry <harrisongitau3@gmail.com>
2023-01-24 16:55:26 +0300

    Fix: Update lipa na mpesa api logic

--- app/routes.py
+++ app/routes.py
@@ -331,30 +331,38 @@ def dashboard_customer_buy_product(id):
     return redirect(url_for('dashboard_customer_checkout'))
 
 
-@app.route('/lipa-na-mpesa')
-def lipa_na_mpesa():
+@app.route('/product/<int:id>/lipa-na-mpesa>')
+@login_required
+def lipa_na_mpesa(id):    
     access_token = mpesa.MpesaAccessToken.validated_mpesa_access_token
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = {'Authorization': 'Bearer %s' % access_token}
     request = {
-        'BusinessShortCode': mpesa.LipaNaMpesaPassword.business_short_code,
-        'Password': mpesa.LipaNaMpesaPassword.decode_online_password,
-        'Timestamp':mpesa.LipaNaMpesaPassword.lipa_time,
+        'BusinessShortCode': mpesa.LipaNaMpesaPassword.business_short_code,   # org receiving funds
+        'Password': mpesa.LipaNaMpesaPassword.decode_online_password,         # used to encrypt the request
+        'Timestamp':mpesa.LipaNaMpesaPassword.lipa_time,                      # transaction time
         'TransactionType': 'CustomerPayBillOnline',
-        'Amount': 1,
-        'PartyA': app.config['MPESA_PARTY_A'],
-        'PartyB': mpesa.LipaNaMpesaPassword.business_short_code,
-        'PhoneNumber': int((current_user.phone).replace('+', '')),
+        'Amount': 1,                                                          # transaction amount
+        'PartyA': int((current_user.phone).replace('+', '')),                 # MSISDN sending the funds
+        'PartyB': mpesa.LipaNaMpesaPassword.business_short_code,               # org receiving the funds
+        'PhoneNumber': int((current_user.phone).replace('+', '')),            # MSISDN sending the funds
         'CallBackURL': 'https://sandbox.safaricom.co.ke/mpesa/',
         'AccountReference': 'primashop',
-        'TransactionDesc': 'testing stk push'
+        'TransactionDesc': 'testing stk push for ecommerce app'
     }
     try:
         response = requests.post(api_url, json=request, headers=headers)
+        print(response.text, f'\n\nStatus: {response.status_code}')
+
+        # Update payment status in database
+        product = PurchasedProducts.query.filter_by(id=id).first_or_404()
+        product.payment_status = True
+        db.session.commit()
+        flash(f'{product.name} paid for.')
+        
     except Exception as e:
-        print(f'Error: \n\n {e}')
-    print(response.text, f'\n\n{response.status_code}')
-    return redirect(url_for('dashboard_customer_checkout'))
+        print(f'Error: \n\n {e}')    
+    return redirect(url_for('dashboard_customer'))
 
 
 # ===========
