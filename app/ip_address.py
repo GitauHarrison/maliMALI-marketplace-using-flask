069941b12321d36262c65f187dd99cd4830fa36a
harry <harrisongitau3@gmail.com>
2023-01-25 08:39:28 +0300

    Feat: Get user location data

--- app/ip_address.py
+++ app/ip_address.py
@@ -9,9 +9,9 @@ def get_user_location():
     handler = ipinfo.getHandler(app.config['IP_INFO_ACCESS_TOKEN'])
     # Get address if user is using a proxy
     # Alternatively use their remote address
-    # ip_user = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    # ip_user = '216.239.36.21'
-    ip_user = request.environ.get('HTTP_X_FORWARDED_FOR')
-    print(ip_user)
+    ip_user = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+
+    # ip_user = '102.222.146.162'
+    # ip_user = request.environ.get('HTTP_X_FORWARDED_FOR')
     details = handler.getDetails(ip_user)
     return details
