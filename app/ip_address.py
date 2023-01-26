79c9dfff56ff7514ecaa07aec2e5b191e8853e32
harry <harrisongitau3@gmail.com>
2023-01-26 19:23:27 +0300

    Fix: Use fixed ip address instead of user actual location

--- app/ip_address.py
+++ app/ip_address.py
@@ -8,13 +8,15 @@ from app import app
 def get_user_location():
     handler = ipinfo.getHandler(app.config['IP_INFO_ACCESS_TOKEN'])
     # Request context
-    with app.test_request_context():
-        from flask import request
+    # with app.test_request_context():
+    #     from flask import request
         # Get address if user is using a proxy
         # Alternatively use their remote address
-        ip_user = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+        #ip_user = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+        # ip_user = request.remote_addr
+        #ip_user = request.remote_addr
 
-    # ip_user = '102.222.146.162'
+    ip_user = '102.222.146.162'
     # ip_user = request.environ.get('HTTP_X_FORWARDED_FOR')
     details = handler.getDetails(ip_user)
     return details
